a790ab42c936dd17b020061a8d6c40658a0519fc
rxfix <panter2004@gmail.com>
2021-05-07 11:38:18 +0300

    11

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ argc = 1489
 s = 0
 while argc != 0:
     cout = argc % 10
-    argc /= 10
-    argc //= 1
+    argc //= 10
     s = s + cout
     print(cout, ',', s)
--- куб.py
+++ куб.py
@@ -8,6 +8,7 @@
 # Внимание: новый список не создавать!!!
 
 
+#  создание списка кубов нечетных чисел от 0 до 1000
 my_list = []  # создаем пустой список
 counter = 0
 while counter <= 1000:
@@ -15,13 +16,44 @@ while counter <= 1000:
         my_list.append(counter ** 3)  # добавить в список куб этого числа
     counter = counter + 1
 print(my_list)
-counter = len(my_list)
-print(counter)
 
 
+# вычисление суммы чисел, сумма цифр которых делится на 7
+counter = len(my_list)  # устанавливаем счетчик на конец списка
+sum_7 = 0  # сумма чисел делящихся нацело на 7
+while counter > 0:
+    sum_numeral = 0  # сумма цифр числа
+    counter = counter - 1
+    number = my_list[counter]  # число из списка
+    while number != 0:  # вычисление суммы цифр числа
+        numeral = number % 10
+        number //= 10
+        sum_numeral = sum_numeral + numeral
+    if sum_numeral % 7 == 0:
+        sum_7 = sum_7 + sum_numeral
+print(sum_7)
+
+
+# доабавление к каждому элементу списка 17
+counter = len(my_list)  # устанавливаем счетчик на конец списка
 while counter > 0:
     counter = counter - 1
     my_list[counter] = my_list[counter] + 17
-    # print(my_list[counter])
 print(my_list)
+
+
+# вычисление суммы чисел, сумма цифр которых делится на 7
+counter = len(my_list)  # устанавливаем счетчик на конец списка
+sum_7 = 0  # сумма чисел делящихся нацело на 7
+while counter > 0:
+    sum_numeral = 0  # сумма цифр числа
+    counter = counter - 1
+    number = my_list[counter]  # число из списка
+    while number != 0:  # вычисление суммы цифр числа
+        numeral = number % 10
+        number //= 10
+        sum_numeral = sum_numeral + numeral
+    if sum_numeral % 7 == 0:
+        sum_7 = sum_7 + sum_numeral
+print(sum_7)
 
